2d228ec2e741f9e768cbefc772e29eea6ac55fc2
Andrea Marruzzo <amarruzzo@gmail.com>
2026-08-08 09:16:30 +0200

    feat: add mentions to JSONL export

--- en/searcher-c2a407aa.js
+++ en/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-018e5c72.js',
+                path_to_root + 'searchindex-50878121.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
